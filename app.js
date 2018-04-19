da472e7c9e7f0e7b546a25885c2fbec7566d0ee3
doarn <draonit2@gmail.com>
2018-04-19 12:43:14 +0800

    Content heseg nevev footer nemev

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.use(express.static(__dirname + '/public'));
 app.set("view engine","ejs")
 app.get("/",(req,res)=>{
   res.render("index");
+  console.log(__dirname);
+});
+app.get('/content',(req,res)=>{
+  res.render('content');
 });
 
 app.listen(3000,()=>{
