0530f7eba5e615127674e7363169ad3ad5127a47
doarn <draonit2@gmail.com>
2018-05-08 19:29:26 +0800

    Structure change

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
 const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser")
 const app = express();
-app.use(express.static(__dirname + '/public'));
-app.set("view engine","ejs")
-app.get("/",(req,res)=>{
-  res.render("index");
-  console.log(__dirname);
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "website"
 });
-app.get('/content',(req,res)=>{
-  res.render('content');
+const query = "select * from posts";
+con.connect(function(err) {
+  if (err) console.log(err);
+  else console.log("Connected!");
 });
 
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set("view engine","ejs")
+app.get("/",(req,res)=>{
+  con.query(query, function (err, result, fields) {
+    if (err) console.log(err);
+    else {
+      res.render("index",{posts:result});
+      console.log(__dirname);
+    }
+  });
+
+});
+app.get('/admin',(req,res)=>{
+  res.render('admin');
+});
+app.post('/admin',(req,res)=>{
+  var title = req.body.title;
+  var url = req.body.url;
+  var text = req.body.description;
+  var sql = `INSERT INTO posts (Title, img, description) VALUE ('${title}','${url}','${text}')`;
+  con.query(sql, function (err, result) {
+    if (err) console.log(err);
+    else console.log("1 record inserted");
+  });
+  res.redirect('/');
+});
+app.get('/login',(req,res)=>{
+  con.query('SELECT * FROM users', function (err, result, fields) {
+    if (err) console.log(err);
+    else {
+      res.render("login",{users:result});
+    }
+  });
+});
 app.listen(3000,()=>{
   console.log(`server running`);
 });
